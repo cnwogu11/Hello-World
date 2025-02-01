6ad94bb8f4dd82b954079e288955c9d9332250d7
NonsoNwogu <chi.nonsonwogu02@gmail.com>
2025-02-01 11:06:10 -0500

    Updated Hello World program

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+// Name: Nonso Nwogu
+// Class: CSET 3600
+//Instructor: Don Miller
+//Homework: 1 - Date: 01/31/2025
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
